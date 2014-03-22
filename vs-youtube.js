df7b527131370ac689ca40932e41167f68a3c223
Andrey Mereskin <mereskin@gmail.com>
2014-03-21 23:19:01 -0600

    rearrange events

--- vs-youtube.js
+++ vs-youtube.js
@@ -1,75 +1,68 @@
-/* requires document, window */
-(function(document, window) {
-  /*
-   * window.YT, window.onYouTubeIframeAPIReady, window.Versal
-   */
-
+(function() {
   var VsYoutubeProto = {
-    createdCallback: function(){
+    attachedCallback: function(){
       this.setAttribute('pending', 'pending');
+
+      if(window.YT && window.YT.loaded) {
+        this.onYoutubeApiReady();
+      }
     },
 
-    attachedCallback: function(){
-      if(window.YT && this.getAttribute('videoid')) {
+    onYoutubeApiReady: function(){
+      var placeholder = document.createElement('div');
+      this.appendChild(placeholder);
+
+      var options = {
+        events: {
+          'onReady': this.onPlayerReady.bind(this),
+          'onStateChange': this.onPlayerStateChange.bind(this)
+        }
+      };
+
+      this.player = new window.YT.Player(placeholder, options);
+    },
+
+    onPlayerReady: function(){
+      this.cueCurrentVideo();
+    },
+
+    onPlayerStateChange: function(e){
+    },
+
+    cueCurrentVideo: function(){
+      if(this.videoId && this.player) {
+        this.player.cueVideoById(this.videoId);
         this.removeAttribute('pending');
       }
     },
 
-    attributeChangedCallback: function(name, old, pending){
-      console.log('attributeChangedCallback', name);
-      if(name === 'pending' && !pending) {
-        this.launch();
-      } else if(name === 'videoid') {
-        this.launch();
-      }
-    },
-
-    launch: function(){
-      if(this.getAttribute('videoId')) {
-        var options = {
-          videoId: this.getAttribute('videoId')
-        };
-
-        this.createPlayerWithOptions(options);
-      }
-    },
-
-    createPlayerWithOptions: function(options){
-      // If player already exists - remove it
-      if(this.player) {
-        this.player.destroy();
-      }
-
-      // YT API replaces the node with iframe
-      var placeholder = document.createElement('div');
-      this.appendChild(placeholder);
-
-      // create a player and store the reference
-      this.player = new window.YT.Player(placeholder, options);
+    attributeChangedCallback: function(name, old, current){
+      if(name == 'video-id') { this.cueCurrentVideo(); }
     }
   };
 
-  function registerYouTubeAPIOnce(){
-    if(window.onYouTubeIframeAPIReady) {
-      console.warn('vs-youtube: Warning! onYouTubeIframeAPIReady is already defined! '+
-        'That should not be happening.');
-    }
+  Object.defineProperty(VsYoutubeProto, 'videoId', {
+    get: function(){ return this.getAttribute('video-id'); },
+    set: function(id) { this.setAttribute('video-id', id); }
+  });
 
+  function registerYouTubeAPIOnce(){
     window.onYouTubeIframeAPIReady = function(){
       resolvePendingPlayers();
     };
-  }
+  };
 
   function resolvePendingPlayers() {
-    var existing = document.querySelectorAll('vs-youtube[pending]');
+    var existing = document.querySelectorAll('vs-youtube');
 
     Array.prototype.forEach.call(existing, function(el) {
-      // console.log(el);
-      el.removeAttribute('pending');
+      if(el.onYoutubeApiReady) {
+        el.onYoutubeApiReady();
+      }
     });
   }
 
   registerYouTubeAPIOnce();
 
   window.Versal.registerElement('vs-youtube', VsYoutubeProto);
-})(document, window);
+})();
